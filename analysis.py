bffdbfd6b4ebf70acec52b48bd225a60b5ad9214
Ruwayda Yusuf <Ruwayda.Yusuf1@outlook.com>
2026-03-06 13:47:47 +0000

    Add initial analysis script using pandas and matplotlib

--- analysis.py
+++ analysis.py
@@ -1,1 +1,20 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Load dataset
+df = pd.read_csv("data/cancers-diagnosed-at-stage-1-or-2-chart-data.csv")
+
+# View first rows
+print(df.head())
+
+# Plot trend
+plt.figure(figsize=(10,6))
+plt.plot(df["Time period"], df["England Value"], marker="o")
+
+plt.title("Early Stage Cancer Diagnosis in England (2013–2022)")
+plt.xlabel("Year")
+plt.ylabel("Percentage Diagnosed at Stage 1 or 2")
+
+plt.grid(True)
+plt.show()
 
